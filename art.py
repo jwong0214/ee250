18ead1a063c50b55dc745f9ab834c6a36f7ebc40
jwong0214 <techatha@usc.edu>
2026-02-04 19:06:49 -0800

    edit art

--- art.py
+++ art.py
@@ -5,7 +5,7 @@ BASE_URL = "https://api.artic.edu/api/v1/artworks/search"
 def search_artworks_by_title(title):
     params = {
         "q": title,
-        "limit": 20,
+        "limit": 5,
         "fields": "id,title,artist_display,date_display"
     }
 
@@ -23,13 +23,18 @@ def search_artworks_by_title(title):
     for art in results:
         artwork_title = art.get("title", "")
 
+        print(f"Title: {artwork_title}")
+        print(f"Artist: {art.get('artist_display', 'N/A')}")
+        print(f"Date: {art.get('date_display', 'N/A')}")
+        print(f"Artwork ID: {art.get('id')}")
+        print("-" * 50)
         # STRICT title-only filter
-        if title.lower() in artwork_title.lower():
-            print(f"Title: {artwork_title}")
-            print(f"Artist: {art.get('artist_display', 'N/A')}")
-            print(f"Date: {art.get('date_display', 'N/A')}")
-            print(f"Artwork ID: {art.get('id')}")
-            print("-" * 50)
+        # if title.lower() in artwork_title.lower():
+        #     print(f"Title: {artwork_title}")
+        #     print(f"Artist: {art.get('artist_display', 'N/A')}")
+        #     print(f"Date: {art.get('date_display', 'N/A')}")
+        #     print(f"Artwork ID: {art.get('id')}")
+        #     print("-" * 50)
 
 
 if __name__ == "__main__":
