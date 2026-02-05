475b562fa10e97e460d812150dc1b4f9668350b7
Janessa Techathamawong <techatha@usc.edu>
2026-02-05 15:46:19 -0800

    Add files via upload

---
+++ tcp_client.py
@@ -0,0 +1,33 @@
+"""
+Server receiver buffer is char[256]
+If correct, the server will send a message back to you saying "I got your message"
+Write your socket client code here in python
+Establish a socket connection -> send a short message -> get a message back -> ternimate
+use python "input->" function, enter a line of a few letters, such as "abcd"
+"""
+import socket
+
+def main():
+    # TODO: Create a socket and connect it to the server at the designated IP and port
+    # TODO: Get user input and send it to the server using your TCP socket
+    # TODO: Receive a response from the server and close the TCP connection
+    # pass
+
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    server_ip = "localhost"
+    server_port = 5000
+    client_socket.connect((server_ip, server_port))
+
+    message = input("input-> ")
+
+    client_socket.send(message.encode())
+
+    response = client_socket.recv(256)
+    print("Server says:", response.decode())
+
+    client_socket.close()
+
+if __name__ == '__main__':
+    main()
+
